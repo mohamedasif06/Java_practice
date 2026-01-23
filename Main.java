ec393c6ec64766c3535cddaa9599cf40a914f8e3
mohamedasif06 <mohamedasif3417@gmail.com>
2026-01-23 22:08:10 +0530

    Two sum java program

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main {
+
+    public int\[] twoSum(int\[] nums, int target) {
+
+        for(int i=0; i< nums.length ; i++){         **// nums.length is used for length of an array**
+
+            for(int j=i+1; j < nums.length ;j++){
+
+                if(nums\[i] + nums\[j] == target){
+
+                    return new int\[]{i,j};         **//creates an array as \[i,j]**
+
+                }
+
+            }
+
+        }
+
+        return new int\[]{};
+
+    }
+
+}
+
+
